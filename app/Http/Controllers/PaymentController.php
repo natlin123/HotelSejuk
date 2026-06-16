1ddd2499f78538454ca62429a3d1e5365ab2ddb1
Sandrina sitorus <sandrinasitorus2019@gmail.com>
2026-06-16 23:54:47 +0700

    membuat isi paymentcontroller

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,8 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Booking;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class PaymentController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $booking = Booking::findOrFail($id);
+
+        return view('booking.payment-details', compact('booking'));
+    }
+
+    public function confirm(Request $request, $id)
+    {
+        $booking = Booking::findOrFail($id);
+
+        $booking->metode_pembayaran =
+            $request->metode_pembayaran;
+
+        $booking->status_pembayaran = 'Success';
+
+        $booking->save();
+
+        return redirect()
+            ->route('payment.success',$booking->id);
+    }
+
+    public function success($id)
+    {
+        $booking = Booking::findOrFail($id);
+
+        return view('booking.success', compact('booking'));
+    }
+
+    public function downloadPdf($id)
+{
+    $booking = Booking::findOrFail($id);
+
+    $pdf = Pdf::loadView(
+        'booking.receipt-pdf',
+        compact('booking')
+    );
+
+    return $pdf->download(
+        'Resi-Pembayaran-'.$booking->kode_transaksi.'.pdf'
+    );
+}
 }
